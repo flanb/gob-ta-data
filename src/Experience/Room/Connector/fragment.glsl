7ef1177dfae863d1c619a0fdd5a6e283b82887cb
FlanB <clement.renou@mmibordeaux.com>
2022-12-07 18:46:30 +0100

    ✨(connector): shaders parameters

--- src/Experience/Room/Connector/fragment.glsl
+++ src/Experience/Room/Connector/fragment.glsl
@@ -2,6 +2,14 @@ uniform vec3 color;
 varying vec3 vPosition;
 varying vec2 vUv;
 
+uniform float uTime;
+uniform float uWaveSpeed;
+uniform float uWaveFrequency;
+
 void main() {
-    gl_FragColor = vec4(color, 1.0);
+    float directionColor = abs(cos(uTime * uWaveSpeed + vUv.x * uWaveFrequency));
+
+    gl_FragColor = vec4(
+    color * directionColor,
+    1.0);
 }
--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -1,14 +1,13 @@
-import * as dat from 'lil-gui'
+import { Pane } from "tweakpane";
+import * as EssentialsPlugin from "@tweakpane/plugin-essentials";
 
-export default class Debug
-{
-    constructor()
-    {
-        this.active = window.location.hash == '#debug'
+export default class Debug {
+  constructor() {
+    this.active = window.location.hash === "#debug";
 
-        if(this.active)
-        {
-            this.gui = new dat.GUI()
-        }
+    if (this.active) {
+      this.gui = new Pane();
+      this.gui.registerPlugin(EssentialsPlugin);
     }
+  }
 }
